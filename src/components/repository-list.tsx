8d38f7161fc31be9951819369237d74f551abb33
Renan Silva <renansilvaytB@gmail.com>
2024-12-09 19:02:02 -0300

    feat: tipagem no fetch de repositorios

--- src/components/repository-list.tsx
+++ src/components/repository-list.tsx
@@ -1,21 +1,22 @@
 import { RepositoryItem } from "./repository-item"
 import '../style/repositories.scss'
 import { useState, useEffect } from "react"
+import { Root, Root2 } from "../@types/repository";
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Root2[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
-    .then(res => res.json())
-    .then(data => setRepositories(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setRepositories(data))
+  }, [])
 
   return (
     <section className="repository-list">
       <h1> Lista de repositórios </h1>
       <ul>
-        {repositories.map(({description, html_url, name, node_id}) => <RepositoryItem key={node_id} description={description} name={name} link={html_url}/>)}
+        {repositories.map(({ description, html_url, name, node_id }) => <RepositoryItem key={node_id} description={description ?? 'Sem descrição'} name={name} link={html_url} />)}
       </ul>
     </section>
   )
